e380bb305dedc3eb56e23a3bfbbb64e26cb22a07
JCTGY <fan3510@gmail.com>
2020-02-06 08:22:46 -0800

    Setup link from pinImages to VC
    * add pinUIviews double array
    * add assginPinColor, need to use the struct later
    * stack rows are replace as previous version(backgroud = white)

--- MasterMind/Controller/ViewController.swift
+++ MasterMind/Controller/ViewController.swift
@@ -25,20 +25,41 @@ class ViewController: UIViewController {
     @IBOutlet var rowTen : [UIButton]?
     var outletButtons = [[UIButton]]()
     var outletIndex = 0
-    var replaceButton : UIButton?
-    var buttons : [UIButton] = []
+    var replaceButton: UIButton?
+    var lastReplaceButton: UIButton?
+    
+    /* =========================== */
+    /*       IBoutlet pinView      */
+    /* =========================== */
+    @IBOutlet var pinRowOne: [UIImageView]?
+    @IBOutlet var pinRowTwo: [UIImageView]?
+    @IBOutlet var pinRowThree: [UIImageView]?
+    @IBOutlet var pinRowFour: [UIImageView]?
+    @IBOutlet var pinRowFive: [UIImageView]?
+    @IBOutlet var pinRowSix: [UIImageView]?
+    @IBOutlet var pinRowSeven: [UIImageView]?
+    @IBOutlet var pinRowEight: [UIImageView]?
+    @IBOutlet var pinRowNine: [UIImageView]?
+    @IBOutlet var pinRowTen: [UIImageView]?
+    var pinUIImageViews = [[UIImageView]]()
+    let pinFillBlack = UIImage(systemName: "pin.fill")?.withTintColor(UIColor.black)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         appendOutLetButtons()
+        appendPinUIImageViews()
     }
     @IBAction func actionForButtons(_ sender: UIButton) {
+        if lastReplaceButton != nil {
+            lastReplaceButton?.setTitle("", for: .normal)
+        }
         replaceButton = sender
         sender.showsTouchWhenHighlighted = true
         sender.setTitle("O", for: .normal)
         sender.setTitleColor(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1), for: .normal)
         sender.backgroundColor = UIColor.white
+        lastReplaceButton = sender
     }
     @IBAction func colorSelectButtons(_ sender: UIButton){
         replaceButton?.setTitle("", for: .normal)
@@ -52,6 +73,7 @@ class ViewController: UIViewController {
                 return ;
             }
         }
+        assignPinColor()
         disableButtons()
         outletIndex = outletIndex < 10 ? outletIndex + 1 : outletIndex
         enableButtons()
@@ -77,6 +99,24 @@ class ViewController: UIViewController {
     }
     func disableButtons() {
         outletButtons[outletIndex].forEach{ $0.isUserInteractionEnabled = false
+        }
+    }
+    func appendPinUIImageViews() {
+        pinUIImageViews.append(pinRowOne!)
+        pinUIImageViews.append(pinRowTwo!)
+        pinUIImageViews.append(pinRowThree!)
+        pinUIImageViews.append(pinRowFour!)
+        pinUIImageViews.append(pinRowFive!)
+        pinUIImageViews.append(pinRowSix!)
+        pinUIImageViews.append(pinRowSeven!)
+        pinUIImageViews.append(pinRowEight!)
+        pinUIImageViews.append(pinRowNine!)
+        pinUIImageViews.append(pinRowTen!)
+    }
+    func assignPinColor() {
+        pinUIImageViews[outletIndex
+        ].forEach {
+            $0.image = pinFillBlack
         }
     }
 
